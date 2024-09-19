0d5b86b53e9433e876b69b973efba3a6130013be
nishanpradhan <nishanpradhan500@gmail.com>
2024-09-19 16:08:58 +0545

    app-bar-component

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -1,3 +1,4 @@
+import 'package:computer_12/views/components/app_bar.dart';
 import 'package:computer_12/views/components/banner_ad_helper.dart';
 import 'package:computer_12/views/topics/topics.dart';
 import 'package:flutter/material.dart';
@@ -8,26 +9,16 @@ class ActionButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 40.0,
-        backgroundColor: const Color(0xff618989),
-        centerTitle: true,
-        title: const Text(
-          'Computer Science',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 25.0,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
+    return const Scaffold(
+      appBar: CustomAppBar(
+        titleText: 'Computer Science',
       ),
-      body: const SizedBox(
+      body: SizedBox(
         child: TopicsList(),
       ),
-      backgroundColor: const Color.fromARGB(130, 173, 223, 241),
-      bottomNavigationBar:const BannerAdHelper(),
-      drawer: const NavDrawer(),
+      backgroundColor: Color.fromARGB(130, 173, 223, 241),
+      bottomNavigationBar: BannerAdHelper(),
+      drawer: NavDrawer(),
     );
   }
 }
